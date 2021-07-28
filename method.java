b2fc510d07a217a4ac0e298e2c424b64310c723d
ゆずき <k.saito70329@gmail.com>
2021-07-28 17:05:56 +0900

    [add] new create methodfile

--- method.java
+++ method.java
@@ -21,8 +21,28 @@ class IsoscelesTriangleLB {
     }
 }
 
+// メソッドの多重定義：類似した処理は同じ名前で多重定義する(必ず仮引数の個数や型は異なるものにする)
 class Max{
+    // maxメソッド1
     static int max(int a, int b) {
+        return a > b ? a:b;
+    }
+    // maxメソッド2
+    static int max(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        return max;
+    }
 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("xの値："); int x = stdIn.nextInt();
+        System.out.print("yの値："); int y = stdIn.nextInt();
+        System.out.print("zの値："); int z = stdIn.nextInt();
+
+        System.out.println("x,yの最大値は" + max(x,y) + "です。"); // maxメソッド1に飛ぶ
+        System.out.println("x,y,zの最大値は" + max(x,y,z) + "です。"); // maxメソッド2に飛ぶ
     }
 }
