f8097696743e3b9043f3aaec3ba30e06e9aefad8
ゆずき <k.saito70329@gmail.com>
2021-08-14 11:29:09 +0900

    [add] new create extendsfile

---
+++ extends.java
@@ -0,0 +1,23 @@
+// 抽象クラス・・・多相性を活用するために使用
+// abstractを付けて、本体{}の代わりに;をおいて宣言する。
+abstract class Shape {
+    abstract void draw(); //抽象メソッド
+}
+class Point extends Shape {
+    Point() {}
+    @Override void draw() {
+        System.out.println('+');
+    }
+}
+class Rectangle extends Shape {
+    private int width;
+    private int height;
+}
+@Override void draw() {
+    for (int i=1; i<=height; i++) {
+        for (int j=1; j<=wigth; j++) {
+            System.out.println('*');
+        }
+        System.out.println();
+    }
+}
